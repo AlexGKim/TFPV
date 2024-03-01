46f57755bace3412e02449abcd357c6277d4a02a
Alex <agkim@lbl.gov>
2024-03-01 11:37:07 -0800

    runs at least

--- fitstojson.py
+++ fitstojson.py
@@ -1,19 +1,49 @@
 import fitsio
 import numpy
 import json
+from astropy.cosmology import Planck18 as cosmo
+import  matplotlib.pyplot as plt
 
 fn = "SGA-2020_iron_Vrot"
 
-fits=fitsio.FITS(fn+".fits")
-data=fits[1].read()
+def to_json():
 
-data_dic=dict()
-for k in data.dtype.names:
-        data_dic[k]=data[k].tolist()
+    Rlim = 17.75
 
-data_dic['N'] = len(data_dic['SGA_ID'])
+    fits=fitsio.FITS(fn+".fits")
+    data=fits[1].read()
 
-json_object = json.dumps(data_dic)
 
-with open(fn+".json", 'w') as f:
-    f.write(json_object)
+    select = data['R_MAG_SB26'] < Rlim
+
+    data_dic=dict()
+    for k in data.dtype.names:
+        if k not in ['SGA_GALAXY','GALAXY','MORPHTYPE','BYHAND','REF','GROUP_NAME','GROUP_PRIMARY','BRICKNAME','D26_REF']:
+            if k in ['G_MAG_SB26_ERR','R_MAG_SB26_ERR','Z_MAG_SB26_ERR']:
+                w=numpy.where(data[k]<0)
+                data[k][w[0]]=1e8
+
+            data_dic[k]=data[k][select].tolist()
+
+    data_dic['N'] = len(data_dic['SGA_ID'])
+    data_dic['Rlim'] = Rlim
+
+
+    data_dic['mu'] = cosmo.distmod(data_dic['Z_DESI']).value.tolist()
+    data_dic['Rlim'] = Rlim
+
+    json_object = json.dumps(data_dic)
+
+    with open(fn+".json", 'w') as f:
+        f.write(json_object)
+
+def plot():
+    fits=fitsio.FITS(fn+".fits")
+    data=fits[1].read()
+    plt.plot(data['Z_DESI'],data['R_MAG_SB26'],'.')
+    plt.xlabel('Z_DESI')
+    plt.ylabel('R_MAG_SB26')
+    plt.show()
+
+if __name__ == '__main__':
+    to_json()
--- tfpv.stan
+++ tfpv.stan
@@ -1,11 +1,49 @@
+functions {
+  vector V_fiber(vector V, vector epsilon) {
+    return V.*cos(epsilon);
+  }
+}
 data {
   int<lower=0> N;
-  array[N] int<lower=0,upper=1> y;
+  real Rlim;
+  vector[N] Z_DESI;
+  vector[N] V_0p4R26;
+  vector[N] V_0p4R26_err;
+  // vector[N] G_MAG_SB26;
+  // vector[N] G_MAG_SB26_ERR;
+  vector[N] R_MAG_SB26;
+  vector[N] R_MAG_SB26_ERR;
+  // vector[N] Z_MAG_SB26;
+  // vector[N] Z_MAG_SB26_ERR;
+  vector[N] mu;
 }
 parameters {
-  real<lower=0,upper=1> theta;
+  // real aG;
+  // real bG;
+  real aR;
+  real bR;  
+  // real aZ;
+  // real bZ;
+  vector<lower=0>[N] V;
+  vector[N] epsilon;
+  // real<lower=0> sigG
+  real<lower=0> sigR;
+  // real<lower=0> sigZ
+  vector[N] mR;
+}
+transformed parameters {
+  vector[N] logV;
+  logV = log10(V);
 }
 model {
-  theta ~ beta(1,1);  // uniform prior on interval 0,1
-  y ~ bernoulli(theta);
+  V_0p4R26 ~ normal(V_fiber(V,epsilon), V_0p4R26_err);
+  // G_MAG_SB26 ~ normal(mu + aG + bG*logV, G_MAG_SB26_ERR);
+  mR ~ normal(mu + aR + bR*logV, sigR);
+  // Z_MAG_SB26 ~ normal(mu + aZ + bZ*logV, Z_MAG_SB26_ERR);
+  // G_MAG_SB26 ~ normal(mu + aG + bG*logV, G_MAG_SB26_ERR);
+  R_MAG_SB26 ~ normal(mR, R_MAG_SB26_ERR);
+  target += - sum(log(erfc(mR-Rlim)));
+  // Z_MAG_SB26 ~ normal(mu + aZ + bZ*logV, Z_MAG_SB26_ERR);
+  epsilon ~ normal(0,pi()/64.);
+  sigR ~ cauchy(0,10);
 }
