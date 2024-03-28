f647a40c3ab6315bf76a4b85f8aa8421903ac1eb
Alex Kim <agkim@lbl.gov>
2024-03-28 09:46:53 -0700

    Works

--- multi.sh
+++ multi.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 for i in {1..10}
 do
-        ./segev sample algorithm=hmc engine=nuts max_depth=15 num_warmup=1000 num_samples=1000 num_chains=4 data file=data/SGA_TFR_simtest_${(l:3::0:)i}.json output file=output/simtest_perp_${(l:3::0:)i}.csv
+        echo ./segev sample algorithm=hmc engine=nuts max_depth=15 adapt delta=0.95 num_warmup=1000 num_samples=1000 num_chains=4 data file=data/SGA_TFR_simtest_${(l:3::0:)i}.json output file=output/simtest_sym_${(l:3::0:)i}.csv
 done
