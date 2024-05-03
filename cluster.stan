36273791eb89331508b643e860569a3c24490142
Alex <agkim@lbl.gov>
2024-05-03 13:08:40 -0700

    iron cluster analysis start

---
+++ cluster.stan
@@ -0,0 +1,160 @@
+// ./iron410 sample algorithm=hmc engine=nuts max_depth=17 adapt delta=0.99 num_warmup=1000 num_samples=1000 num_chains=4 init=data/SGA-2020_iron_Vrot_cuts_sub_0.10_init.json data file=data/SGA-2020_iron_Vrot_cuts_sub_0.10.json output file=output/iron_410_cuts_sub_0.10.csv
+
+
+// functions {
+//   vector V_fiber(vector V, vector epsilon) {
+//     return V./cos(epsilon);
+//   }
+// }
+
+data {
+  int<lower=0> N;
+
+  int<lower=0> N_cluster;
+  array[N_cluster] int N_per_cluster;
+  array[N_cluster] real mu;
+
+  vector[N] V_0p4R26;
+  vector[N] V_0p4R26_err;
+  vector[N] R_MAG_SB26;
+  vector[N] R_MAG_SB26_ERR;
+
+  // vector[N] Rhat;
+  // vector[N] Vhat;
+  // real Rhat_noise;
+  // vector[N] Vhat_noise;  
+
+  //for iron
+  vector[N] mu;
+  // vector[N] dm_v;
+  real Rlim;
+  vector[N] Rlim_eff;
+  real Vmin;
+  real Vmax;
+
+  real omega_dist_init;
+  real xi_dist_init;
+}
+
+transformed data {
+
+  // 4 cases
+  // 1 : line fit only
+  // 2 : log-V dispersion
+  // 3 : mag dispersion
+  // 4 : perp dispersion
+  int dispersion_case=4;
+
+  int pure = 1;
+  int angle_error = 0;
+
+  int flatDistribution = 0;
+
+  vector[N] dR = sqrt(R_MAG_SB26_ERR.*R_MAG_SB26_ERR + Rhat_noise*Rhat_noise);
+  vector[N] dV = sqrt(V_0p4R26_err.*V_0p4R26_err + Vhat_noise.*Vhat_noise);
+
+  // real dwarf_mag=-17. + 34.7;
+
+  // Kelly finds standard deviation between 14.2 deg between MANGA and SGA
+  // real angle_dispersion_deg = 14.2;
+  // real angle_dispersion_deg = 5.;
+  // real angle_dispersion = angle_dispersion_deg/180*pi();
+
+}
+
+
+// from eyeball look at data expect b ~ -7.1, a ~ -6.1
+// average logV ~ 2.14
+parameters {
+  // vector<lower=0, upper=pi()/4>[N] epsilon;    // angle error. There is a 1/cos so avoid extreme
+
+  vector[N] logL_raw;       // latent parameter
+  // if (flatDistribution == 0)
+  // {
+  // parameters for SkewNormal
+  real<lower=-10, upper=0> alpha_dist;
+  real<lower=0.5, upper=4> omega_dist;  
+  real<lower=12, upper=18> xi_dist;
+  // }
+
+  real<lower=atan(-9) , upper=atan(-5.5)> atanAR; // negative slope positive cosine
+
+  real bR;
+  vector[N_cluster] bR_offset;
+
+  vector[N] random_realization_raw;
+  real<lower=0> sigR;
+}
+model {
+
+
+  vector[N] logL;
+  if (flatDistribution==0) {
+    logL=omega_dist*logL_raw+xi_dist;
+  } else {
+    logL=logL_raw*omega_dist_init + xi_dist_init;
+  } 
+  vector[N] random_realization=random_realization_raw*sigR;
+  real sinth = sin(atanAR);
+  real costh = cos(atanAR);
+
+ // slope of redsidual dispersion
+  real sinth_r; real costh_r; real sinth2_r; real costh2_r; 
+  if (dispersion_case == 1)
+  {
+    sinth_r=0; costh_r=0; // sinth2_r=0; costh2_r=0;
+  }
+  else if (dispersion_case ==2)
+  {
+    sinth_r=1; costh_r=0; //sinth2_r=1; costh2_r=0;
+  }
+  else if (dispersion_case ==3)
+  {
+    sinth_r=0; costh_r=1; //sinth2_r=0; costh2_r=1;
+  }
+  else if (dispersion_case==4)
+  {
+    sinth_r=-costh; costh_r=sinth; //sinth2_r=-costh2; costh2_r=sinth2;
+  }
+  else if (dispersion_case==5)
+  {
+    // sinth_r=sin(atanARr); costh_r=cos(atanARr); //sinth2_r=-costh2; costh2_r=sinth2;
+  }
+
+  // velocity model with or without axis error
+  vector[N] VtoUse = pow(10, costh*logL  + random_realization*costh_r );
+  if (angle_error == 1){
+      // VtoUse = V_fiber(VtoUse,epsilon);
+  } 
+
+  vector[N] m_realize;
+  int index=1;
+  for (i in 1:N_cluster){
+    for (j in 1:N_per_cluster[i]){
+      m_realize[i]= bR_offset[i] +  mu[i]+ sinth * logL[index]  + random_realization[index]*sinth_r;
+      i=i+1;
+    }
+  }
+  m_realize = bR + m_realize;
+
+  R_MAG_SB26 ~ normal(m_realize, R_MAG_SB26_ERR) T[,Rlim];
+  V_0p4R26 ~ normal(VtoUse, V_0p4R26_err) T[Vmin,Vmax];
+
+  if (flatDistribution==0)
+  {
+      logL_raw ~ skew_normal(0, 1 ,alpha_dist);
+  } else {
+      logL_raw ~ normal(0, 10);
+  }
+
+  random_realization_raw ~ normal (0, 1);
+  sigR ~ cauchy(0.,10);
+
+  bR_offset ~ normal(0,10);
+
+  // if (angle_error==1)
+  //   epsilon ~ normal(0,angle_dispersion);
+}
+generated quantities {
+   real aR=tan(atanAR);
+}
--- fitstojson.py
+++ fitstojson.py
@@ -3,7 +3,9 @@ import numpy
 import json
 from astropy.cosmology import Planck18 as cosmo
 import  matplotlib.pyplot as plt
-
+import glob
+import pandas
+from astropy.table import Table
 
 fn = "SGA-2020_iron_Vrot_cuts"
 fn_sga = "data/SGA-2020_fuji_Vrot"
@@ -196,6 +198,133 @@ def to_json(frac=1, cuts=False):
     with open("data/"+outname2, 'w') as f:
         f.write(json.dumps(init))
 
+
+def iron_cluster_json():
+    fn = "SGA-2020_iron_Vrot"
+
+    Rlim = 17.75-0.1
+    Mlim = -17.
+    Vmin = 70
+    Vmax = 300
+    cosi = 1/numpy.sqrt(2)
+    q0=0.2
+    balim = numpy.sqrt(cosi**2 * (1-q0**2) + q0**2)
+
+    table = Table.read("data/"+fn+".fits")
+    pv_df = table.to_pandas()
+    print(pv_df.dtypes)
+    # Some things to apply to N_all
+
+    # read in the cluster files
+    N_cluster = []
+    mu = []
+
+    V_0p4R26 = []
+    V_0p4R26_err = []
+    R_MAG_SB26 = []
+    R_MAG_SB26_ERR = []
+    Rlim_eff = []
+    Rlim_eff #  = numpy.minimum(Rlim, mu+Mlim)
+
+   # selection effects
+    for fn in glob.glob("data/output_*.txt"):
+        df = pandas.read_csv(fn)
+        print(df.dtypes)
+        N_cluster.append(df.shape[0])
+        combo_df = df.merge(pv_df, on='SGA_ID')
+
+
+    # # add extra noise degrading data to help fit
+    # dt = {'names':['Vhat','Vhat_noise','Rhat'], 'formats':[float, float,float]}
+    # extradata = numpy.zeros(len(data['Z_DESI']),dtype=dt)
+    # extradata['Vhat_noise'] = 0.02*data["V_0p4R26"]
+    # Rhat_noise = 0.1
+    # extradata['Vhat'] = numpy.random.normal(loc=data["V_0p4R26"], scale=extradata['Vhat_noise'])
+    # extradata['Rhat'] = numpy.random.normal(loc=data['R_MAG_SB26'], scale=Rhat_noise)
+
+    select = numpy.logical_and.reduce(data['R_MAG_SB26'] < Rlim_eff  , data['Vhat'] > Vmin, data['Vhat'] < Vmax, data["BA"] < balim)
+
+    data_dic=dict()
+    for k in data.dtype.names:
+        if k not in ['SGA_GALAXY','GALAXY','MORPHTYPE','BYHAND','REF','GROUP_NAME','GROUP_PRIMARY','BRICKNAME','D26_REF']:
+            if k in ['G_MAG_SB26_ERR','R_MAG_SB26_ERR','Z_MAG_SB26_ERR']:
+                w=numpy.where(data[k]<0)
+                data[k][w[0]]=1e8
+
+            data_dic[k]=data[k][select].tolist()
+
+    for k in extradata.dtype.names:
+        data_dic[k]=extradata[k][select].tolist()
+
+    data_dic['mu'] = cosmo.distmod(data_dic['Z_DESI']).value.tolist()
+    data_dic['Rlim_eff'] = Rlim_eff.tolist()
+    z = numpy.array(data_dic["Z_DESI"])
+    dv = 300
+    dm = (5/numpy.log(10)*(1+z)**2*dv/cosmo.H(z)/cosmo.luminosity_distance(z)).value
+    data_dic['dm_v'] = dm.tolist()
+
+    N_all = len(data_dic['Z_DESI'])
+    if frac !=1 :
+        ind = numpy.random.randint(0, high=N_all, size=int(N_all*frac))
+        for key, value in data_dic.items():
+            value=numpy.array(value)[ind]
+            data_dic[key] = value.tolist()
+
+    data_dic['N'] = len(data_dic['SGA_ID'])
+    data_dic['Rlim'] = Rlim
+    data_dic['Mlim'] = Mlim
+    data_dic['Vmin'] = Vmin
+    data_dic['Vmax'] = Vmax
+
+    data_dic['Rhat_noise'] = Rhat_noise
+
+    data_dic["alpha_dist_init"]=-2.8782
+    data_dic["xi_dist_init"]= 16.556
+    data_dic["omega_dist_init"]=1.4521 
+
+    json_object = json.dumps(data_dic)
+
+    cstr=""
+    if cuts:
+        cstr="_cuts"
+    if frac==1:
+        outname = fn+cstr+".json"
+        outname2 = fn+cstr+"_init.json"
+    else:
+        outname =  fn+cstr+"_sub_{:4.2f}.json".format(frac)
+        outname2 = fn+cstr+"_sub_{:4.2f}_init.json".format(frac)
+
+    with open("data/"+outname, 'w') as f:
+        f.write(json_object)
+
+#  vector[N] v = 373.137*v_raw + 222.371;
+    init = dict()
+
+    init["atanAR"] = numpy.arctan(-6.9878)
+    init['bR'] = -5.3173
+    init['sigR'] = 0.07
+    logL = numpy.log10(data_dic["V_0p4R26"])/numpy.cos(init["atanAR"])
+
+    if cuts:
+        # init["alpha_dist"]=-2.4813505391290436
+        # init["xi_dist"]= 14.628796578863792
+        # init["omega_dist"]=1.4880837674710605
+        init["alpha_dist"]=data_dic["alpha_dist_init"]
+        init["xi_dist"]= data_dic["xi_dist_init"]
+        init["omega_dist"]=data_dic["omega_dist_init"]
+    else:
+        init["alpha_dist"]=-3.661245022462153
+        init["xi_dist"]= 14.913405242237685
+        init["omega_dist"]=2.2831016215521247
+
+    # init["mu_dist"]=13.133570672711606
+    # init["sigma_dist"]= 1.5160651053079683
+    init["logL_raw"]  = ((logL-init["xi_dist"])/init["omega_dist"]).tolist()
+
+    init["dv"] = (numpy.zeros(data_dic['N'])).tolist()
+    init["random_realization_raw"] = (numpy.zeros(data_dic['N'])).tolist()
+    with open("data/"+outname2, 'w') as f:
+        f.write(json.dumps(init))
     
 
 def segev_json(fn='SGA_TFR_simtest_20240307'):
@@ -264,7 +393,8 @@ def segev_plot(fn = fn_segev2):
 
 if __name__ == '__main__':
     # to_json(frac=0.1,cuts=True)
-    coma_json(cuts=True)
+    # coma_json(cuts=True)
+    iron_cluster_json()
     # for i in range(1,11):
     #     segev_json("data/SGA_TFR_simtest_{}".format(str(i).zfill(3)))
     # # segev_plot()
