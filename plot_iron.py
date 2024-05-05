fix coma to have jacobian
Convert optimal to randomized initial condition

plot_iron.py
@@ -127,10 +127,34 @@ with open(fn, 'r') as f:
 with open("output/cluster_410_opt.csv", newline='') as csvfile:
     optimal4 = pandas.read_csv(csvfile,comment='#')
 
+optimal4_ran = optimal4.copy()
+optimal4_ran.loc[0] = numpy.random.normal(optimal4_ran.loc[0],numpy.abs(optimal4_ran.loc[0])*0.1)
+optimal4_ran.loc[0,"atanAR"] = numpy.random.normal(optimal4.loc[0]["atanAR"],numpy.abs(optimal4.loc[0]["atanAR"])*0.01)
+ratio=numpy.cos(optimal4.loc[0]["atanAR"])/numpy.cos(optimal4_ran.loc[0]["atanAR"])
+optimal4_ran.loc[0,"xi_dist"] = optimal4.loc[0]["xi_dist"]*ratio
+optimal4_ran.loc[0,"omega_dist"] = optimal4.loc[0]["omega_dist"]*ratio
+optimal4_ran.loc[0].to_json("data/cluster_410_opt.json")
 
-MR = numpy.array(data["R_MAG_SB26"]) - numpy.array(data["mu_all"])
 brArr4=numpy.array([optimal4["bR.{}".format(i+1)][0] for i in range(0,data["N_cluster"])])
 brArr4mn= brArr4-brArr4.mean()
+
+with open("output/cluster_310_opt.csv", newline='') as csvfile:
+    optimal3 = pandas.read_csv(csvfile,comment='#')   
+optimal3_ran = optimal3.copy()
+optimal3_ran.loc[0] = numpy.random.normal(optimal3_ran.loc[0],numpy.abs(optimal3_ran.loc[0])*0.1)
+
+optimal3_ran.loc[0,"atanAR"] = numpy.random.normal(optimal3.loc[0]["atanAR"],numpy.abs(optimal3.loc[0]["atanAR"])*0.01)
+ratio=numpy.cos(optimal3.loc[0]["atanAR"])/numpy.cos(optimal3_ran.loc[0]["atanAR"])
+optimal3_ran.loc[0,"xi_dist"] = optimal3.loc[0]["xi_dist"]*ratio
+optimal3_ran.loc[0,"omega_dist"] = optimal3.loc[0]["omega_dist"]*ratio
+optimal3_ran.loc[0].to_json("data/cluster_310_opt.json")
+
+brArr3=numpy.array([optimal3["bR.{}".format(i+1)][0] for i in range(0,data["N_cluster"])])
+brArr3mn= brArr3-brArr3.mean()
+
+
+MR = numpy.array(data["R_MAG_SB26"]) - numpy.array(data["mu_all"])
+
 index = 0
 for i in range(0,data["N_cluster"]): #range(data["N_cluster"]):
     if True:
@@ -154,22 +178,21 @@ plt.xscale('log',base=10)
 plt.xlabel("V_0p4R26")
 plt.ylabel(r"R_MAG_SB26-$\mu$")
 plt.ylim((MR.max()+.5,MR.min()-.5))
+plt.title("Perpendicular Best Fit")
 plt.show()
 
-with open("output/cluster_310_opt.csv", newline='') as csvfile:
-    optimal3 = pandas.read_csv(csvfile,comment='#')   
-brArr3=numpy.array([optimal3["bR.{}".format(i+1)][0] for i in range(0,data["N_cluster"])])
-brArr3mn= brArr3-brArr3.mean()
+
 index = 0
 for i in range(0,data["N_cluster"]): #range(data["N_cluster"]):
     if True:
         plt.errorbar(data["V_0p4R26"][index:index+data["N_per_cluster"][i]], MR[index:index+data["N_per_cluster"][i]] ,yerr=data["R_MAG_SB26_ERR"][index:index+data["N_per_cluster"][i]],xerr=data["V_0p4R26_err"][index:index+data["N_per_cluster"][i]], fmt=".")
     index = index+data["N_per_cluster"][i]
-plt.plot(plt.xlim(), brArr3.mean()+numpy.log10(plt.xlim())*optimal2['aR'][0])
+plt.plot(plt.xlim(), brArr3.mean()+numpy.log10(plt.xlim())*optimal3['aR'][0])
 plt.xscale('log',base=10)
 plt.xlabel("V_0p4R26")
 plt.ylabel(r"R_MAG_SB26-$\mu$")
 plt.ylim((MR.max()+.5,MR.min()-.5))
+
 plt.show()
 
  
@@ -184,6 +207,7 @@ plt.xscale('log',base=10)
 plt.xlabel("V_0p4R26")
 plt.ylabel(r"R_MAG_SB26-$\mu$")
 plt.ylim((MR.max()+.5,MR.min()-.5))
+plt.title("Inverse TF Best Fit")
 plt.show()
 
 
@@ -193,6 +217,8 @@ print(ans) # (-1.3565289337241162, 14.193371687903761, 1.0984767423119663)
 
 plt.hist(numpy.log10(data["V_0p4R26"]),density=True)
 x=numpy.linspace(1.7,2.6,100)
-plt.plot(x, scipy.stats.norm.pdf(x/numpy.cos(optimal4["atanAR"][0]),  optimal4["xi_dist"][0], optimal4["omega_dist"][0])/numpy.cos(optimal4["atanAR"][0]))
-plt.plot(x, scipy.stats.norm.pdf(x/numpy.cos(optimal3["atanAR"][0]), optimal3["xi_dist"][0], optimal3["omega_dist"][0])/numpy.cos(optimal3["atanAR"][0]))
+plt.plot(x, scipy.stats.norm.pdf(x/numpy.cos(optimal4["atanAR"][0]),  optimal4["xi_dist"][0], optimal4["omega_dist"][0])/numpy.cos(optimal4["atanAR"][0]),label="Perpendicular")
+plt.plot(x, scipy.stats.norm.pdf(x/numpy.cos(optimal3["atanAR"][0]), optimal3["xi_dist"][0], optimal3["omega_dist"][0])/numpy.cos(optimal3["atanAR"][0]),label="Inverse TF")
+plt.legend()
+plt.xlabel(r"$\log{(V\_0p4R26)}$")
 plt.show()