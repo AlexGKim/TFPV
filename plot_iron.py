fix double projection on sigR calculation

plot_iron.py
@@ -17,7 +17,7 @@ def cluster():
 
     chains=[]
     c = ChainConsumer()
-
+    c2 = ChainConsumer()
     fig_b, ax_b = plt.subplots()
     fig_b2, ax_b2 = plt.subplots()
     for _ in [3,4]:
@@ -30,6 +30,10 @@ def cluster():
         for df_ in dum:
             df_["bR_use"] = df_[bRcols].mean(axis=1) - df_["xi_dist"]*df_["aR"]
             df_["omega_dist_use"] = df_["omega_dist"] * numpy.cos(df_["atanAR"])
+            if _==3:
+                df_['sigR_proj'] = df_['sigR']
+            elif _==4:
+                df_['sigR_proj'] = -numpy.sin(df_["atanAR"])*df_["sigR"]
         dum=pandas.concat(dum)
         chains.append(dum)
         # dum=pandas.read_csv("output/temp_{}.csv".format(1),comment='#')
@@ -62,13 +66,13 @@ def cluster():
 
         # c = ChainConsumer()
         c.add_chain(Chain(samples=dum[["aR","bR_use","sigR","xi_dist","omega_dist_use"]], name=name))
-
+        c2.add_chain(Chain(samples=dum[["aR","bR_use","sigR","xi_dist","omega_dist_use","sigR_proj"]], name=name))
 
         if _==3:
-            _v = numpy.percentile(dum["aR"]*dum["sigR"],(32,50,100-32))
+            _v = numpy.percentile(dum["sigR"],(32,50,100-32))
         elif _==4:
-            _v = numpy.percentile(dum["aR"]*numpy.sin(dum["atanAR"])*dum["sigR"],(32,50,100-32))
-        print("${:4.2f}_{:4.2f}^+{:4.2f}$".format(_v[1], -_v[1]+_v[0],_v[2]-_v[1]))      
+            _v = numpy.percentile(numpy.sin(dum["atanAR"])*dum["sigR"],(32,50,100-32))
+        print("${:5.3f}_{:5.3f}^+{:5.3f}$".format(_v[1], -_v[1]+_v[0],_v[2]-_v[1]))      
 
         # fig = c.plotter.plot()
         # plt.savefig("corner_cluster_{}.png".format(_))
@@ -103,6 +107,7 @@ def cluster():
     # plt.show()
     plt.clf()
     print(c.analysis.get_latex_table())
+    print(c2.analysis.get_latex_table())
 
 
 
@@ -165,6 +170,7 @@ wfe
 def fuji():
     chains=[]
     c = ChainConsumer()
+    c2 = ChainConsumer()
     for _ in [3,4]:
         if _ == 3:
             name = 'Inverse TF'
@@ -174,18 +180,24 @@ def fuji():
         for df_ in dum:
             df_["bR_use"] = df_["bR"] - df_["xi_dist"]*df_["aR"]
             df_["omega_dist_use"] = df_["omega_dist"] * numpy.cos(df_["atanAR"])
+            if _==3:
+                df_['sigR_proj'] = df_['sigR']
+            elif _==4:
+                df_['sigR_proj'] = -numpy.sin(df_["atanAR"])*df_["sigR"]
+
         dum=pandas.concat(dum)
         chains.append(dum)
         # dum=pandas.read_csv("output/temp_{}.csv".format(1),comment='#')
 
         # c = ChainConsumer()
         c.add_chain(Chain(samples=dum[["aR","bR_use","sigR","xi_dist","omega_dist_use"]], name=name))
-
-        if _==3:
-            _v = numpy.percentile(dum["aR"]*dum["sigR"],(32,50,100-32))
-        elif _==4:
-            _v = numpy.percentile(dum["aR"]*numpy.sin(dum["atanAR"])*dum["sigR"],(32,50,100-32))
-        print("${:4.2f}_{:4.2f}^+{:4.2f}$".format(_v[1], -_v[1]+_v[0],_v[2]-_v[1]))      
+        c2.add_chain(Chain(samples=dum[["aR","bR_use","sigR","xi_dist","omega_dist_use","sigR_proj"]], name=name))
+    #     if _==3:
+    #         _v = numpy.percentile(dum["sigR"],(32,50,100-32))
+    #     elif _==4:
+    #         _v = numpy.percentile(numpy.sin(dum["atanAR"])*dum["sigR"],(32,50,100-32))
+    #     print("${:5.3f}_{:5.3f}^+{:5.3f}$".format(_v[1], -_v[1]+_v[0],_v[2]-_v[1]))      
+    # qwd
 
     c.set_plot_config(
         PlotConfig(
@@ -198,6 +210,7 @@ def fuji():
     # plt.show()
     plt.clf()
     print(c.analysis.get_latex_table())
+    print(c2.analysis.get_latex_table())
 
     fn="data/SGA-2020_fuji_Vrot_cuts.json"
     fn_all="data/SGA-2020_fuji_Vrot.json"