5499840ebf3990069a410640087c5e85a06fe928
Alex <agkim@lbl.gov>
2026-03-08 11:38:57 -0700

    make for gou

---
+++ ariel/test_gpu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -A desi_g
+#SBATCH -C gpu
+#SBATCH -q debug
+#SBATCH -t 0:30:00
+#SBATCH -n 1
+#SBATCH -c 32
+#SBATCH --gpus-per-task=1
+
+#export SLURM_CPU_BIND="cores"
+
+srun ./tophat sample num_warmup=50 num_samples=50 num_chains=1 data file=output/c000_ph000_r000/input.json init=output/c000_ph000_r000/init.json \
+    output file=output/c000_ph000_r000/tophat.csv
